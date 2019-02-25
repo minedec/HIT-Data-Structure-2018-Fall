89081f2eb245ec74183073de497fe48c7f04b2ad
Jiang-JD <37043133+Jiang-JD@users.noreply.github.com>
2019-02-25 20:51:15 +0800

    实验1
    上传初始文件，未修改

---
+++ 软件构造/lab1/Lab1-1163450201/src/MagicSquare.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class MagicSquare {
+	public static void main(String[] args)
+	{
+		isLegalMagicSquare("./src/P1/1.txt");
+		isLegalMagicSquare();
+		isLegalMagicSquare();
+		isLegalMagicSquare();
+		isLegalMagicSquare();
+	}
+	
+	boolean isLegalMagicSquare(String fileName)
+	{
+		
+	}
+}
